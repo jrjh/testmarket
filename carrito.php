bfdd1d5630a769c9426bc9e506164bae614cfffc
Jose Raul <rulo_jh@hotmail.com>
2021-01-06 13:42:09 -0700

    alert

--- carrito.php
+++ carrito.php
@@ -3,16 +3,8 @@ session_start();
 
 $mensage="";
 
-if(isset($_POST['add'])){
-switch($_POST['add']){
-    case 'Agregar':
-        if(is_numeric($_POST['id'])){
-            $id=$_POST['id'];
-            $mensage=$id;
-        }
-}
-}
-/*
+
+
 $nombre=$_POST['nombre'];
 $cantidad=$_POST['cantidad'];
 $precio=$_POST['precio'];
@@ -36,5 +28,5 @@ else{
     );
     $_SESSION['CARRITO'][$numeroproductos]=$producto;
 }
-print_r($_SESSION,true);*/
+$mensage= $nombre $cantidad $precio;
 ?>
