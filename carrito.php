0355529d277bb9896286e3f83b28d8dda6a99e2b
Jose Raul <rulo_jh@hotmail.com>
2021-01-06 14:15:05 -0700

    switch

--- carrito.php
+++ carrito.php
@@ -4,32 +4,41 @@ session_start();
 $mensage="";
 
 if(isset($_POST['add'])){
-    $id=$_POST['id'];
-$nombre=$_POST['nombre'];
-$cantidad=$_POST['cantidad'];
-$precio=$_POST['precio'];
+    switch($_POST['add']){
 
-if(!isset($_SESSION['CARRITO'])){
-$producto=array(
-    'ID'=>$id,
-    'NOMBRE'=>$nombre,
-    'CANTIDAD'=>$cantidad,
-    'PRECIO'=>$precio
-);
-$_SESSION['CARRITO'][0]=$producto;
-}
-else{
-    $numeroproductos=count($_SESSION['CARRITO']);
-    $producto=array(
-        'ID'=>$id,
-        'NOMBRE'=>$nombre,
-        'CANTIDAD'=>$cantidad,
-        'PRECIO'=>$precio
-    );
-    $_SESSION['CARRITO'][$numeroproductos]=$producto;
-}
+        case'Agregar':
+            $id=$_POST['id'];
+        $nombre=$_POST['nombre'];
+        $cantidad=$_POST['cantidad'];
+        $precio=$_POST['precio'];
+        
+        if(!isset($_SESSION['CARRITO'])){
+        $producto=array(
+            'ID'=>$id,
+            'NOMBRE'=>$nombre,
+            'CANTIDAD'=>$cantidad,
+            'PRECIO'=>$precio
+        );
+        $_SESSION['CARRITO'][0]=$producto;
+        }
+        else{
+            $numeroproductos=count($_SESSION['CARRITO']);
+            $producto=array(
+                'ID'=>$id,
+                'NOMBRE'=>$nombre,
+                'CANTIDAD'=>$cantidad,
+                'PRECIO'=>$precio
+            );
+            $_SESSION['CARRITO'][$numeroproductos]=$producto;
+
+           
+        }
+        $mensage=print_r($_SESSION,true);
+        break;
+    }
+    
 
 }
 
-$mensage=print_r($_SESSION,true);
+
 ?>
