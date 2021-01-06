4d3abbad1fbdb9abae8f3cfbb2b8d15a00c6f263
Jose Raul <rulo_jh@hotmail.com>
2021-01-06 16:30:19 -0700

    tables

--- showcarro.php
+++ showcarro.php
@@ -12,26 +12,23 @@ $(document).ready( function () {
 } );
 </script>
 
-<table id='tablee' class="table table-striped table-inverse table-responsive">
-    <thead class="thead-inverse">
+<table id="tablee" class="display">
+    <thead>
         <tr>
-            <th>titulo</th>
-            <th></th>
-            <th></th>
+            <th>Column 1</th>
+            <th>Column 2</th>
         </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td scope="row">text</td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td scope="row"></td>
-                <td></td>
-                <td></td>
-            </tr>
-        </tbody>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Row 1 Data 1</td>
+            <td>Row 1 Data 2</td>
+        </tr>
+        <tr>
+            <td>Row 2 Data 1</td>
+            <td>Row 2 Data 2</td>
+        </tr>
+    </tbody>
 </table>
 
 <?php include 'templates/pie.php';?>
